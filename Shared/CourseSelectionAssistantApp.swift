366e8688a5e0c0d4ee62067cb1af30e135395183
Russell Gordon <rgordon@lcs.on.ca>
2021-03-17 14:14:27 -0400

    Initial Commit

---
+++ Shared/CourseSelectionAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseSelectionAssistantApp.swift
+//  Shared
+//
+//  Created by Russell Gordon on 2021-03-17.
+//
+
+import SwiftUI
+
+@main
+struct CourseSelectionAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
